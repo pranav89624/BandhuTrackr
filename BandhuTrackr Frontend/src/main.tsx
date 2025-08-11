f90132c14704d483ad005bd312299b46b918f790
Pranav Verma <pranav12340987@gmail.com>
2025-08-11 22:08:33 +0530

    feat: set clerk in frontend

--- BandhuTrackr Frontend/src/main.tsx
+++ BandhuTrackr Frontend/src/main.tsx
@@ -2,11 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 // Render app immediately
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
   </StrictMode>
 );
 
